ed73881195561c7b0dc399c77b4a853ea7a03dcc
julienoyer <juue@live.fr>
2021-07-06 14:14:10 +0200

    ajout 10+13

--- exo_10.rb
+++ exo_10.rb
@@ -1,7 +1,7 @@
 puts "En quelle année es-tu né(e) ?"
 n = gets.chomp.to_i
 
-for i in (n..2021) do
-    puts "Année : #{i}"
-    puts "Age #{2021 - i}"
-  end
+for i in n..2021  
+  puts "Année : #{i}" 
+  puts "Age #{i - n}"
+end
--- exo_13.rb
+++ exo_13.rb
@@ -1,10 +1,11 @@
+
 emails = Array.new
 
-for i in 0..50
-    if i < 9
-    emails [i]="jean.dupont.0#{i+1}@email.fr "
-    else 
-    emails [i]="jean.dupont.#{i+1}@email.fr "
+for i in 0...50
+    if i<9
+        emails[i] = "jean.dupont.0#{i+1}@email.fr"
+    else
+     emails[i] = "jean.dupont.#{i+1}@email.fr"
+    end 
+    p emails[i]
 end
-end
-
